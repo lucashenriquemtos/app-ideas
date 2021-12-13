a8183b2f5252974879f3bcb7bfc2c53983aaa163
lucasmatos <lucasmatos@pactosolucoes.com.br>
2021-12-13 18:10:34 -0300

    bin2dec

---
+++ app-ideas_UI/src/app/bin2dec/bin2dec.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Bin2decComponent } from './bin2dec.component';
+
+
+
+@NgModule({
+  declarations: [
+    Bin2decComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class Bin2decModule { }
