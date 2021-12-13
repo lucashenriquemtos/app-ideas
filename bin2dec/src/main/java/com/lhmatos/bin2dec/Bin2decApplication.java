961ef8f14d8f8ba1ce96e96b2b99d30551520fd0
lucasmatos <lucasmatos@pactosolucoes.com.br>
2021-12-13 17:44:24 -0300

    Inicial

---
+++ bin2dec/src/main/java/com/lhmatos/bin2dec/Bin2decApplication.java
@@ -0,0 +1,13 @@
+package com.lhmatos.bin2dec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bin2decApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Bin2decApplication.class, args);
+    }
+
+}
---
+++ bin2dec/src/test/java/com/lhmatos/bin2dec/Bin2decApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lhmatos.bin2dec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bin2decApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
